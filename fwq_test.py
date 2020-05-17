a32a0ee2e9c3f9cfda453997bffea867dab6c940
boczjs <boczjs@hotmail.com>
2020-05-17 22:03:16 +0800

    test

---
+++ fwq_test.py
@@ -0,0 +1,18 @@
+'''
+@Author: boczjs
+@Date: 2020-05-17 21:56:28
+@LastEditTime: 2020-05-17 21:58:57
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \myobject\fwq_test.py
+'''
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+a=np.arange(1,48,1)
+h=2
+for n in range(len(a)):
+    n=n+1
+    if n==2:
+        break
+print(n)
--- test.md
+++ test.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: boczjs
  * @Date: 2020-05-14 20:48:16
- * @LastEditTime: 2020-05-14 22:47:14
+ * @LastEditTime: 2020-05-17 21:41:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myobject\myobject\readme.md
@@ -9,3 +9,12 @@
 hello world
 --i like python
 aaaaa
+bbb
+ccc
+ddd
+eee
+fff
+hhh
+jjj
+ddd
+
